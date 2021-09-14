ea62c6ef547f0ec54d842e8c0317e0717c01cae9
Boris-The-Slav <75560716+Boris-The-Slav@users.noreply.github.com>
2021-09-14 21:25:05 +0200

    games json finished

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const helmet = require("helmet");
+const fs = require("fs");
+const path = require("path");
+
+
+const gamesPath = path.join(process.cwd(), "/data", "/games.json");
+const games = fs.readFileSync(gamesPath, { encoding: "utf-8" });
+
+const app = express();
+const PORT = process.env.PORT || "3000";
+const HOST = process.env.HOST || "0.0.0.0";
+
+app.use(helmet());
+
+app.use("/api/games",(req,res) => {
+    res.status(200).send(games)
+})
+
+app.listen(PORT,HOST,()=> {
+    console.log(`Server is running on port :${PORT}`)
+})
