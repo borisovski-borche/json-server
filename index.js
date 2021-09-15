2baba2beece57a77d173ae7dbdcac76644d4e62c
Boris-The-Slav <75560716+Boris-The-Slav@users.noreply.github.com>
2021-09-15 08:50:10 +0200

    added cors because angular ...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const fs = require("fs");
 const path = require("path");
-
+const cors = require("cors");
 
 const gamesPath = path.join(process.cwd(), "/data", "/games.json");
 const games = fs.readFileSync(gamesPath, { encoding: "utf-8" });
@@ -12,13 +12,14 @@ const PORT = process.env.PORT || "3000";
 const HOST = process.env.HOST || "0.0.0.0";
 
 app.use(helmet());
+app.use(cors());
 
-app.use("/api/games",(req,res) => {
-    res.set("Content-Type","application/json")
+app.use("/api/games", (req, res) => {
+  res.set("Content-Type", "application/json");
 
-    res.status(200).send(games)
-})
+  res.status(200).send(games);
+});
 
-app.listen(PORT,HOST,()=> {
-    console.log(`Server is running on port :${PORT}`)
-})
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on port :${PORT}`);
+});
