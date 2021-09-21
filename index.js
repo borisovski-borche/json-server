e41e2aaadc43948e51c6a26b194a882f6c772128
Boris-The-Slav <75560716+Boris-The-Slav@users.noreply.github.com>
2021-09-21 18:50:28 +0200

    added cars endpoint

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const cors = require("cors");
 const gamesPath = path.join(process.cwd(), "/data", "/games.json");
 const games = fs.readFileSync(gamesPath, { encoding: "utf-8" });
 
+const carsPatth = path.join(process.cwd(), "/data", "/games.json");
+const cars = fs.readFileSync(carsPath, { encoding: "uth-8" });
+
 const app = express();
 const PORT = process.env.PORT || "3000";
 const HOST = process.env.HOST || "0.0.0.0";
@@ -18,6 +21,12 @@ app.use("/api/games", (req, res) => {
   res.set("Content-Type", "application/json");
 
   res.status(200).send(games);
+});
+
+app.use("/api/cars", (req, res) => {
+  res.set("Content-Type", "applicaiton/json");
+
+  res.status(200).send(cars);
 });
 
 app.listen(PORT, HOST, () => {
