18a20205d98881d0a8f9bf1abb1f66d85802e135
borisovski-borche <75560716+borisovski-borche@users.noreply.github.com>
2022-02-17 21:24:15 +0100

    added students.json

--- index.js
+++ index.js
@@ -10,12 +10,21 @@ const games = fs.readFileSync(gamesPath, { encoding: "utf-8" });
 const carsPath = path.join(process.cwd(), "/data", "/cars.json");
 const cars = fs.readFileSync(carsPath, { encoding: "utf-8" });
 
+const studentsPath = path.join(process.cwd(), "/data", "/students.json");
+const students = fs.readFileSync(studentsPath, { encoding: "utf-8" });
+
 const app = express();
 const PORT = process.env.PORT || "3000";
 const HOST = process.env.HOST || "0.0.0.0";
 
 app.use(helmet());
 app.use(cors());
+
+app.use("/api/students", (req, res) => {
+  res.set("Content-Type", "application/json");
+
+  res.status(200).send(students);
+});
 
 app.use("/api/games", (req, res) => {
   res.set("Content-Type", "application/json");
